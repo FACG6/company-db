683920989d5c36879adb466ca53958cc919cf0d9
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-02-12 16:23:57 +0200

    fix bugs in database

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -3,9 +3,9 @@ const path = require('path');
 const fs = require('fs');
 const dbConnection = require('./db_connection');
 
-const sql = fs.readFileSync(path.join(__dirname, './db_build.sql')).toString();
+const sql = fs.readFileSync(path.join(__dirname, 'db_build.sql')).toString();
 
 dbConnection.query(sql, (err, res) => {
-  if (err) throw new Error('Error during building the database');
+  if (err) throw err;
   console.log('Database is ready');
 });
