41ced0fce0608ad8605059dc2355923850bee793
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-02-12 15:16:17 +0200

    reformat code

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,14 +1,19 @@
-const fetch = ({ method, url, value, callback }) => {
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const fetch = ({
+  method, url, value, callback,
+}) => {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4)
+    if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         callback(null, JSON.stringify(xhr.responseText));
       } else {
         callback(
-          new Error(`Server responded with status code of ${xhr.status}`)
+          new Error(`Server responded with status code of ${xhr.status}`),
         );
       }
+    }
   };
   xhr.open(method, url);
   xhr.send(value);
