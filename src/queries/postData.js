300f709d200d2f4c724aaebe009dc8a4ef666b59
Anies12 <a2nies2012@gmail.com>
2019-02-13 14:15:50 +0200

    resolved conflicts

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -7,9 +7,7 @@ const postData = (data, cb) => {
     'Accounting and Finance': 3,
     'Human resources management': 4,
   };
-  dbconnections.query(`INSERT INTO employees (employee_name, employee_salary, department_key) VALUES ('${data.name}','${data.salary}','${deptIDS[data.departments]}');`, (err, res) => {
-    console.log(1111111111111, err);
-    
+  dbconnections.query(`INSERT INTO employees (employee_name,employee_age, employee_salary, department_key) VALUES ('${data.name}','${data.age}','${data.salary}','${deptIDS[data.departments]}');`, (err, res) => {
     if (err) { cb(err); } else {
       cb(null, 'Employee Data add successfully');
     }
