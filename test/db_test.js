d4482d07568e29d12eb44142bfa8a27cf07287db
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-13 16:33:59 +0200

    add test of postData function

--- test/db_test.js
+++ test/db_test.js
@@ -1,39 +1,53 @@
 const tape = require('tape');
-const runDbBuild=require('../src/database/db_build');
- const { getDepartmentData, getEmployeeData, getDemptEmplys }=require('../src/queries/getData.js');
-
-
-
-tape('test getDepartmentData ',(t)=>{
-  runDbBuild((err, res) =>{    
+const runDbBuild = require('../src/database/db_build');
+const { getDepartmentData, getEmployeeData, getDemptEmplys } = require('../src/queries/getData.js');
+const postData=require('../src/queries/postData.js');
+tape('test getDepartmentData ', (t) => {
+  runDbBuild((err, res) => {
     if (err) t.error(err, 'Error');
-    const expected = [ { department_name: 'Managment', department_description: 'the adminstration dpt.' }, { department_name: 'Marketing', department_description: 'the delling promoting dpt.' }, { department_name: 'Accounting and Finance', department_description: 'the planning, organizing and finance management dept' }, { department_name:'Human resources management', department_description: 'Recruitment and training dpt.' } ]
-    getDepartmentData((err, result)=>{
+    const expected = [{ department_name: 'Managment', department_description: 'the adminstration dpt.' }, { department_name: 'Marketing', department_description: 'the delling promoting dpt.' }, { department_name: 'Accounting and Finance', department_description: 'the planning, organizing and finance management dept' }, { department_name: 'Human resources management', department_description: 'Recruitment and training dpt.' }];
+    getDepartmentData((err, result) => {
       if (err) t.error(err);
-      t.deepEqual(result,expected,'test is getDepartmentData done');
+      t.deepEqual(result, expected, 'test is getDepartmentData done');
       t.end();
-    })
-
-  })
+    });
+  });
 });
 
-tape('test getDemptEmplys ',(t)=>{
-  runDbBuild((err, res) =>{    
+tape('test getDemptEmplys ', (t) => {
+  runDbBuild((err, res) => {
     if (err) t.error(err, 'Error');
-    let expected=[ { employee_name: 'ahmed', employee_age: 19, employee_salary: 28.9, department_name: 'Managment' }, { employee_name: 'ahmedd', employee_age: 19, employee_salary: 28.9, department_name: 'Managment' }, { employee_name: 'jaaa', employee_age: 19, employee_salary: 28.9, department_name: 'Managment' } ];
+    const expected = [{
+ employee_name: 'ahmed', employee_age: 19, employee_salary: 28.9, department_name: 'Managment' 
+}, {
+ employee_name: 'ahmedd', employee_age: 19, employee_salary: 28.9, department_name: 'Managment' 
+}, {
+ employee_name: 'jaaa', employee_age: 19, employee_salary: 28.9, department_name: 'Managment' 
+}];
 
-    getDemptEmplys('Managment',(error, result)=>{
-      
-      if (error)
-         t.error(error);
+    getDemptEmplys('Managment', (error, result) => {
+      if (error) {t.error(error);}
 
-      t.deepEqual(result,expected,'test done');
+      t.deepEqual(result, expected, 'test getDemptEmplys done');
       t.end();
-    })
-
-  })
+    });
+  });
 });
 
-tape.onFinish(()=>{
+
+tape('test  postData Emplys ', (t) => {
+  runDbBuild((err, res) => {
+    if (err) t.error(err, 'Error');
+    const expected =  'Employee Data add successfully';
+
+   postData ({name:'testname', age:12, salary:555, departments:'Managment'}, (error, result) => {
+      if (error) {t.error(error);}
+      t.equal(result, expected, 'test of  postData is done');
+      t.end();
+    });
+  });
+});
+
+tape.onFinish(() => {
   process.exit(0);
 });
