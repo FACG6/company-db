c9d1c06f7e9d0f086410145b9cb6c1f0cc373487
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-13 10:56:22 +0200

    add test

--- test/db_test.js
+++ test/db_test.js
@@ -1,7 +1,15 @@
 const tape = require('tape');
 const runDbBuild=require('../test/db_test.js');
- const postData=requir()
+
+//  const postData=requir('/');
  tape('testing tape', (t) => {
   t.equal(1, 1, 'pass');
   t.end();
 });
+
+tape('test get ',(t)=>{
+  runDbBuild((error,res)=>{
+    if (error)console.log(error);
+    
+  })
+})
