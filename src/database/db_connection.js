834be1cec970407833d615a5b36eda0745c48fba
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-13 15:47:06 +0200

    change the url of test database

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -2,14 +2,14 @@ const { Pool } = require('pg');
 const url = require('url');
 require('dotenv').config();
 
-let DB_URL = process.env.DB_URL;
+let DATABASE_URL = process.env.DATABASE_URL;
 
 if (process.env.NODE_ENV === 'test') {
-  DB_URL = process.env.DB_URL_TEST;
+  DATABASE_URL = process.env.HEROKU_POSTGRESQL_SILVER_URL;
 }
-if (!DB_URL) throw new Error ('there is no url');
+if (!DATABASE_URL) throw new Error ('there is no url');
 
-const params = url.parse(DB_URL);
+const params = url.parse(DATABASE_URL);
 const [userName, password] = params.auth.split(':');
 const options = {
   host: params.hostname,
